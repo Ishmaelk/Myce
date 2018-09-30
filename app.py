79b4bfe160cff15818bec96a8c292a2b57500c48
Jonathan Trachtenberg <jonathancodesaway@gmail.com>
2018-09-29 19:33:02 -0700

    working on app.py, databases

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ import sys
 
 app = Flask(__name__)
 
-#engine = create_engine('mysql+pymysql://')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/jon/Myce/test.db'
 
+db = SQLAlchemy(app)
 
+class Test(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	use = db.Column(db.String(200))
+	text = db.Column(db.String(200))
+	complete = db.Column(db.String(200))
+
+db.create_all()
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+	lines = db.session.query(Test).all()
+	return render_template('index.html', lines=lines)
 
 if __name__ == '__main__':
     app.run(debug=True)
